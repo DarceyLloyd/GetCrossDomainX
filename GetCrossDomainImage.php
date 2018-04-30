4188c0752aec0d3bb3dc8403dcab12e5db6ed601
Darcey <Darcey.Lloyd@gmail.com>
2018-04-30 06:17:07 +0100

    1.0.1
    1.0.1

--- GetCrossDomainImage.php
+++ GetCrossDomainImage.php
@@ -1,49 +1,6 @@
 <?php
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function trace($arg){ echo("<span style='border:1px solid #CCCCCC'>".$arg."</span><br>"); }
-function boolToString($arg){
-	if ($arg){
-		return("true");
-	} else {
-		return("false");
-	}
-}
-function track($src){
-	$filename = "GetImageStats.trk";
-	$count = file_get_contents($filename);
-	if ($count == null){
-		$count = 0;
-	}
-
-	$count++;
-	$handle = fopen($filename, "w+");
-	fwrite($handle, $count);
-	fclose($handle);
-
-
-	$handle = fopen("./log/data.csv", "a");
-	$dte = date("d-m-Y H:i:s");
-	$ref = $_SERVER['HTTP_REFERER'];
-	$ip = $_SERVER['REMOTE_ADDR'];
-
-	$matches = "codepen";
-	$match = strpos($matches, $ref);
-	// $data = [$count,$dte,gettype($match),boolToString($match),$ref,$src];
-	if (strpos($ref,$matches) === false) {
-		$data = [$count,$dte,$ip,$ref,$src];
-	} else {
-		$data = [$count,$dte,$ip,"codepen.io",$src];
-	}
-
-	// foreach ($_SERVER as $key => $value) {
-	// 	$v = $key . "=" . $value;
-	// 	array_push($data,$v);
-	// 	// echo($key . " = " . $value . "\n\r");
-	// }
-	// trace($data);
-	fputcsv($handle, $data);
-	fclose($handle);
-}
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
@@ -57,8 +14,6 @@ if (isset($_GET["src"])){
 	echo("please use script correctly!");
 	die();
 }
-
-track($src);
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
